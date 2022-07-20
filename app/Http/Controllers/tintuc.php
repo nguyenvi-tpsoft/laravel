c7eaeaea54d640afd02869426d6e5d9f63999db0
ViNguyen laptop <vicntt10@gmail.com>
2022-07-20 23:52:22 +0700

    create

---
+++ app/Http/Controllers/tintuc.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class tintuc extends Controller
+{
+    // trang chu
+    public function index()
+    {
+        return view('pages.tintuc');
+    }
+}
---
+++ resources/views/pages/tintuc.blade.php
@@ -0,0 +1,6 @@
+@extends('index')
+@section('pages')
+
+
+<p>Hello tin tuwcs</p>
+@endsection
